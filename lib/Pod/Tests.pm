f4a1b08b72def12599850d61834f6d676192f249
Karen Etheridge <ether@cpan.org>
2019-03-02 11:59:31 -0800

    bump version to 1.20

--- lib/Pod/Tests.pm
+++ lib/Pod/Tests.pm
@@ -52,13 +52,8 @@ embarking on this.
 
 use 5.005;
 use strict;
-use vars qw($VERSION);
-BEGIN {
-	$VERSION = '1.19';
-}
 
-
-
+our $VERSION = '1.20';
 
 
 #####################################################################
